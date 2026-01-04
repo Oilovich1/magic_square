e9f1a9601bb0c3f211409a57375af8585c1a2d1d
Oilovich1 <mailliam909@gmail.com>
2026-01-04 14:28:57 +0500

    осатлось добавит ввод и вывод из файла

--- main.cpp
+++ main.cpp
@@ -26,23 +26,47 @@ int main() {
             printSquareWithDelay(square, 100);
         }
             break;
-        case 2:
+        case 2:{
             std::cout << "Вы выбрали: Загрузить магический квадрат из файла.\n";
-            break;
-        case 3:
+        }
+             break;
+        case 3:{
             std::cout << "Вы выбрали: Проверить является ли текущий квадрат магическим.\n";
+            if (isMagicSquare(square)) {
+                std::cout << "Текущий квадрат магический\n";
+            }
+            else {
+                std::cout << "Текщий квадрат НЕ магический\n";
+            }
+        }
             break;
-        case 4:
+        case 4:{
             std::cout << "Вы выбрали: Вывести текущий квадрат.\n";
+            printSquareWithDelay(square, 100);
+        }
             break;
-        case 5:
+        case 5:{
             std::cout << "Вы выбрали: Показать суммы строк, столбцов и дагоналей\n";
+            std::vector<int> rows = getRowSums(square);
+            std::vector<int> cols = getColSums(square);
+            std::pair<int, int> diagonals = getDiagonalSums(square);
+            for (int i = 0; i < size; i++) {
+                std::cout << "Сумма значений " << i + 1 << " ряда = " << rows[i] << std::endl;
+            }
+            for (int j = 0; j < size; j++) {
+                std::cout << "Сумма значений " << j + 1 << " столбца = " << cols[j] << std::endl;
+            }
+            std::cout << "Сумма значений на главной диагонали = " << diagonals.first << std::endl;
+            std::cout << "Сумма значений на побочной диагонали = " << diagonals.second << std::endl;
+        }
             break;
-        case 6:
+        case 6:{
             std::cout << "Вы выбрали: Сохранить текущий квадрат в файл\n";
+        }
             break;
-        case 7:
+        case 7:{
             std::cout << "Выход из программы";
+        }
             break;
         default:
             std::cout << "Неизвестная команда.\n";
